86fa114cad7a11b6e255862b142e6f8cbf836c78
Ayush Kumawat <55251054+Ayush9549@users.noreply.github.com>
2022-12-09 10:13:40 +0530

    Add files via upload

---
+++ ui.js
@@ -0,0 +1,12 @@
+const selectedElement = document.getElementById("screen");
+
+function show(key)
+{
+	if (screen_text==='volumebtn')
+	{
+		screen_text = 
+	}
+
+
+	selectedElement.innerHTML=screen_text;	
+}
